f9cba2775b282c07b91cc06b1640065b3306bdd6
Mihai Petcu <mihai.costin.petcu@gmail.com>
2016-03-23 15:06:29 +0200

    Update cron.php

--- app/cron.php
+++ app/cron.php
@@ -6,46 +6,54 @@
 use Phalcon\DI\FactoryDefault\CLI as CliDI,
     Phalcon\CLI\Console as ConsoleApp;
 
-// Using the CLI factory default services container
-$di = new CliDI();
+try {
 
-// Define path to application directory
-defined('APP_PATH') || define('APP_PATH', realpath(dirname(__FILE__)));
 
-// Load necessary
-$loader = new \Phalcon\Loader();
-$loader->registerDirs(
-    array(
-        APP_PATH.'/tasks',
-        APP_PATH.'/models',
-        APP_PATH.'/library'
-    )
-);
-$loader->register();
+    // Using the CLI factory default services container
+    $di = new CliDI();
 
-// Load the conf
-if (is_readable(APP_PATH . '/config/config.php')) {
-    $config = include APP_PATH . '/config/config.php';
-    $di->set('config', $config);
-}
-// Mongo connection
-$di->set('mongo', function () use ($config) {
-    if (!$config->mongo->user OR !$config->mongo->pass) {
-        $mongo = new MongoClient('mongodb://' . $config->mongo->host);
-    } else {
-        $mongo = new MongoClient("mongodb://" . $config->mongo->user . ":" . $config->mongo->pass . "@" . $config->mongo->host, array("db" => $config->mongo->dbname));
+    // Define path to application directory
+    defined('APP_PATH') || define('APP_PATH', realpath(dirname(__FILE__)));
+
+    // Load necessary
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(
+        array(
+            APP_PATH . '/tasks',
+            APP_PATH . '/models',
+            APP_PATH . '/library',
+            APP_PATH . '/library/PHPExcel/Classes/'
+        )
+    );
+    $loader->register();
+
+    // Load the conf
+    if (is_readable(APP_PATH . '/config/config.php')) {
+        $config = include APP_PATH . '/config/config.php';
+        $di->set('config', $config);
     }
-    return $mongo->selectDb($config->mongo->dbname);
-}, false);
+    // Mongo connection
+    $di->set('mongo', function () use ($config) {
+        if (!$config->mongo->user OR !$config->mongo->pass) {
+            $mongo = new MongoClient('mongodb://' . $config->mongo->host);
+        } else {
+            $mongo = new MongoClient("mongodb://" . $config->mongo->user . ":" . $config->mongo->pass . "@" . $config->mongo->host, array("db" => $config->mongo->dbname));
+        }
+        return $mongo->selectDb($config->mongo->dbname);
+    }, false);
 
-// Load collection nanager
-$di->set('collectionManager', function(){
-    return new \Phalcon\Mvc\Collection\Manager();
-});
+    // Load collection nanager
+    $di->set('collectionManager', function () {
+        return new \Phalcon\Mvc\Collection\Manager();
+    });
 
-// Create a console app
-$console = new ConsoleApp();
-$console->setDI($di);
+    // Create a console app
+    $console = new ConsoleApp();
+    $console->setDI($di);
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
 try {
     $console->handle();
